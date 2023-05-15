cb0e7977fa63e4091cb754a6b033be5a48c249b0
master <pnayimbila@st.ug.edu.gh>
2023-05-15 13:24:35 +0000

    Add a boilerplate to the app.js file.

---
+++ app.js
@@ -0,0 +1,21 @@
+
+//Require installed modules
+const express = require("express");
+const bodyParser = require("body-parser");
+
+//Stores the content of express in the app variable
+const app = express();
+
+//Uses express to access local files
+app.use(express.static("public"));
+
+//Uses body-parser to encoded the json response received from an API or a form data
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/", function(req, res){
+    res.send("I have started work!");
+});
+
+app.listen(3000, function(){
+    console.log("Server is running on port 3000");
+});
