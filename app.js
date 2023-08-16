2d70d61f306efcf136f8c0cc3314c93f546f4602
PercyAyimbilaNsolemna <pnayimbila@st.ug.edu.gh>
2023-08-16 20:42:17 +0000

    Modified the code.

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
 
 //Connects to mongodb 
-const URL = 'mongodb+srv://percy:Ayimbila@cluster0.az84zbp.mongodb.net/todolistDB?retryWrites=true&w=majority';
+const URL = 'mongodb+srv://userName:<Password>@cluster0.az84zbp.mongodb.net/todolistDB?retryWrites=true&w=majority';
 mongoose.connect(URL);
 console.log('Successfully connected to mongodb');
 
